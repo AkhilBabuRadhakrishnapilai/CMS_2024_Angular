d2ae5eade5ed3db2b115a428138896693d2dfa88
Emma <emma@gmail.com>
2024-07-05 22:47:24 +0530

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { StockComponent } from './stock/stock.component';
+import { SharedComponent } from './shared/shared.component';
 
 const routes: Routes = [
+  { path:'stock',component:StockComponent,loadChildren : ()=>import('./stock/stock.module')
+    .then(x=>x.StockModule)
+  },
+  {path:'shared',component:SharedComponent,loadChildren : ()=>import('./shared/shared.module')
+    .then(x=>x.SharedModule)
+  },
   {
     path:'admin',component:AdminComponent,
     loadChildren:()=>import('./admin/admin.module')
